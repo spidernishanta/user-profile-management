e39ecfa12a5a911b987f98af820c37bc91a44007
spidernishanta <nishantann@gmail.com>
2025-03-29 23:47:43 +0530

    user model added

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  address: { type: String, required: true },
+  password: { type: String, required: true },
+  bio: { type: String },
+  profilePic: { type: String },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10); //password hashing
+  }
+  next();
+});
+
+module.exports = mongoose.model("User", userSchema);
